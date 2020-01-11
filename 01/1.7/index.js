62404a3ec32da5c98888d67bb60497d174e9ff2e
LuzAlejandraGonzalez <lgonzalez@unitedvirtualities.com>
2020-01-11 15:13:47 -0700

    Finished Module 1: Node != JavaScript

---
+++ 01/1.7/index.js
@@ -0,0 +1,8 @@
+/* const data = require('data');
+console.log(data);
+
+const config = require('./config.json');
+console.log(config); */
+
+const addon = require('addon');
+console.log(addon.hello());
