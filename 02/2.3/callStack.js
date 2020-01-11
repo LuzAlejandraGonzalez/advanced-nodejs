f07f868d66cabdcebe8cf8d8937ad1d87dc1f297
LuzAlejandraGonzalez <lgonzalez@unitedvirtualities.com>
2020-01-11 15:14:55 -0700

    Finished Module 2: Concurrency Model and Event Loop

---
+++ 02/2.3/callStack.js
@@ -0,0 +1,12 @@
+// The Call Stack
+const add = (a, b) => a + b;
+
+const double = a =>
+    add(a,a);
+
+const printDouble = a => {
+    const output = double(a);
+    console.log(output);
+};
+
+printDouble(9);
---
+++ 02/2.6/setImmediate.js
@@ -0,0 +1,12 @@
+// setInmediate and process.nextTick
+
+const fs = require('fs');
+
+fs.readFile(__filename,()=> {
+    setTimeout(() => {
+        console.log('timeout');
+    },0);
+    setImmediate(()=>{
+        console.log('immediate');
+    });
+});
